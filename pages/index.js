8b541a21e928fefb9dffdac384d0476238c75adc
Kumaraguru <19eucs071@skcet.ac.in>
2021-10-03 16:37:06 +0530

    adding faq

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import About from "../components/About";
 import Collaborators from "../components/Collaborators";
+import FAQ from "../components/FAQ";
 import Hero from "../components/Hero";
 import Leaderboard from "../components/Leaderboard";
 import Navbar from "../components/Navbar";
@@ -22,6 +23,7 @@ export default function Home() {
       <Stats />
       <Collaborators />
       <Leaderboard />
+      <FAQ />
     </div>
   );
 }
