6648534839efab316d32fde428127df8f0a65a58
Kumaraguru <19eucs071@skcet.ac.in>
2021-10-03 15:31:35 +0530

    adding dashboard, project submission and remvoing local history

--- pages/submit.js
+++ pages/submit.js
@@ -2,16 +2,17 @@ import Head from "next/head";
 import Image from "next/image";
 import About from "../components/About";
 import Collaborators from "../components/Collaborators";
-import DashboardHero from "../components/DashboardHero";
 import EventsCard from "../components/EventsCard";
 import EventsHero from "../components/EventsHero";
 import Hero from "../components/Hero";
 import Leaderboard from "../components/Leaderboard";
 import Navbar from "../components/Navbar";
+import ProfileComponent from "../components/Profile";
 import Stats from "../components/Stats";
+import SubmitProject from "../components/SubmitProject";
 import styles from "../styles/Home.module.css";
 
-export default function Dashboard() {
+export default function Profile() {
   return (
     <div>
       <Head>
@@ -20,7 +21,7 @@ export default function Dashboard() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Navbar />
-      <DashboardHero />
+      <SubmitProject />
     </div>
   );
 }
