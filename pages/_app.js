2cbcf5e1647921b59d350e9a96df8e3fa764556e
Bhuvi100 <bhuvi@kajuzi.co.za>
2021-10-03 22:58:40 +0530

    Add auth to index page

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,15 @@
 import "../styles/globals.css";
 import { ChakraProvider } from "@chakra-ui/react";
+import {supabase} from "../utils/supabaseClient";
 
 function MyApp({ Component, pageProps }) {
+    function passLoggedIn() {
+        return supabase.auth.user() !== null;
+    }
+
   return (
     <ChakraProvider>
-      <Component {...pageProps} />
+      <Component {...pageProps } loggedIn={passLoggedIn()} />
     </ChakraProvider>
   );
 }
