56f841026a5e7a09ccfc07c33ad514c08327f22f
Bhuvi100 <bhuvi@kajuzi.co.za>
2021-10-04 01:35:12 +0530

    Use _app.js for navbar props

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,29 @@
 import "../styles/globals.css";
 import { ChakraProvider } from "@chakra-ui/react";
 import {supabase} from "../utils/supabaseClient";
+import Navbar from "../components/Navbar";
+import Router from "next/router";
 
 function MyApp({ Component, pageProps }) {
-    function passLoggedIn() {
+    function isLoggedIn() {
         return supabase.auth.user() !== null;
+    }
+
+    async function handleLogin() {
+        let { error } = await supabase.auth.signIn({ provider: "github" });
+        if (error) console.log("Error: ", error.message);
+    }
+
+    async function handleLogout() {
+        let { error } = await supabase.auth.signOut();
+        if (error) console.log("Error: ", error.message);
+
+        await Router.push('/');
     }
 
   return (
     <ChakraProvider>
-      <Component {...pageProps } loggedIn={passLoggedIn()} />
+      <Component {...pageProps } loggedIn={isLoggedIn()} handleLogin={handleLogin} handleLogout={handleLogout}/>
     </ChakraProvider>
   );
 }
